8450319f027dfdd367145310010206f8bd7bd4fd
Ever <ever864@gmail.com>
2021-06-27 22:51:56 +0000

    Add method select_warrior to game.rb file

--- game.rb
+++ game.rb
@@ -1,7 +1,10 @@
+require_relative "characters"
 # frozen_string_literal: true
-
 # Game
 class Game
+
+include Catalog
+
   def initialize
     puts ''
   end
@@ -9,6 +12,7 @@ class Game
   def start
     show_welcome
     show_message
+    select_warrior
   end
 
   def show_welcome
@@ -28,6 +32,20 @@ class Game
     puts 'long-lasting memories, fight the shady organization, the Broken Covenant, '
     puts 'and do much more!'
   end
+
+  def select_warrior
+    select_type = Catalog::CHARACTERS.select{|_name,data| data[:type] == "player"}
+    
+    type = ""
+    until select_type.has_key? type
+      puts "Con qué personaje quieres luchar?","\n"
+      puts "     Warrior or Magician     ","\n"
+      puts "date: puts your warrior in downcase."
+      type = gets.chomp.downcase
+    end
+
+  end
+
 end
 
 game = Game.new
