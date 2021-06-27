484a800f875c296588e8081f8fb0b34be5f9c7b2
David Ortiz <david.ortiz.p@gmail.com>
2021-06-27 20:45:19 +0000

    Catalog added

---
+++ characters.rb
@@ -0,0 +1,40 @@
+module Catalog
+  CHARACTERS = {
+    "warrior" => {
+      type: "player",
+      base_exp: 50,
+      base_stats: { hp: 90, speed: 45 },
+      moves: %w[sword\ attack] # ["sword","attack"] >> ["sword attack"]
+    },
+    "magician" => {
+      type: "player",
+      base_exp: 50,
+      base_stats: { hp: 70, speed: 65 },
+      moves: %w[magic\ wind]
+    },
+    "lockheed" => {
+      type: "bot",
+      base_exp: 50,
+      base_stats: { hp: 45, speed: 50 },
+      moves: %w[byte fire\ breathing]
+    },
+    "drogon" => {
+      type: "bot",
+      base_exp: 50,
+      base_stats: { hp: 90, speed: 60 },
+      moves: %w[byte fire\ breathing]
+    },
+    "godzilla" => {
+      type: "bot",
+      base_exp: 50,
+      base_stats: { hp: 180, speed: 45 },
+      moves: %w[atomic\ breath]
+    },
+    "smaug" => {
+      type: "bot",
+      base_exp: 50,
+      base_stats: { hp: 200, speed: 65 },
+      moves: %w[fire\ breathing confussion]
+    }
+  }.freeze
+end
