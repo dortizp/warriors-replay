38a196e2d5a7d988a136b41b52e4268e202e7256
Ever <ever864@gmail.com>
2021-06-28 01:39:24 +0000

    Resolved rubocop offenses

--- bot.rb
+++ bot.rb
@@ -1,13 +1,12 @@
-require_relative "characters"
+# frozen_string_literal: true
 
+require_relative 'characters'
+# select_random_dragon
 class Bot
-
-def select_random_dragon 
-    select_dragons = Catalog::CHARACTERS.select{|_name,type| type[:type] =="bot"}
-    select_key_dragon = select_dragons.collect{|key,value|key}
+  def select_random_dragon
+    select_dragons = Catalog::CHARACTERS.select { |_name, type| type[:type] == 'bot' }
+    select_key_dragon = select_dragons.collect { |key, _value| key }
     select_dragon = select_key_dragon.sample
     puts select_dragon
-end
-
-
+  end
 end
